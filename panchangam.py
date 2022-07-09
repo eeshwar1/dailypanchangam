38263e545d7786839d43cb85b3616c0c8159fc64
Venky Venkatakrishnan <venkyul@gmail.com>
2022-07-09 15:27:02 -0500

    Add files via upload

--- panchangam.py
+++ panchangam.py
@@ -1,128 +1,215 @@
+import tkinter as tk
+from datetime import date, timedelta
+
 import requests
 from bs4 import BeautifulSoup
-import tkinter as tk
-from datetime import date
 
-def find_data(str_type):
+class Panchangam(tk.Tk):
     
-    return_text = ""
-    results = soup.find(text=str_type).parent
-       
-    if results.name != "span":
-        results = results.parent
-
-    spans = results.find_next_siblings("span")
-
-    for span in spans:
-        return_text += span.text
+    def __init__(self, *args, **kwargs):
         
-    return return_text
+       tk.Tk.__init__(self, *args, **kwargs)
+       
+       self.geometry("800x360")
+       self.date = date.today()
+       
+       self.container = dayFrame(self, self.date)
 
-def showPrevDate():
-    print("Prev Date")
+       self.container.grid(row=1,column=1,columnspan=4)
+       
+       self.title("Daily Panchangam")
+       self.resizable(False, False) # disable resizing
     
-    return
+       frmBtnPrev = tk.Frame(master=self)
+       frmBtnPrev.grid(row=8,column=1,padx=5, pady=5)
+       btnPrev = tk.Button(master=frmBtnPrev, text="<", command=self.showPrevDate)
+       btnPrev.pack()
+       
+       frmBtnNext = tk.Frame(master=self)
+       frmBtnNext.grid(row=8,column=4,padx=5, pady=5)
+       btnNext = tk.Button(master=frmBtnNext, text=">", command=self.showNextDate)
+       btnNext.pack()
+        
+        
+    def showPrevDate(self):
+        
+        self.date -= timedelta(days = 1)
+        
+        self.container.set_date(self.date)
+       
+        return
 
-
-def showNextDate():
-    print("Next Date")
+    def showNextDate(self):
+        
+        self.date += timedelta(days = 1)
+     
+        self.container.set_date(self.date)
+        
+        return
     
-    return
+class dayFrame(tk.Frame):
 
-# URL = "https://www.drikpanchang.com/panchang/month-panchang.html?geoname-id=4684888&date=07/07/2022"
-
-today = date.today().strftime("%d/%m/%Y")
-
-URL = "https://www.drikpanchang.com/tamil/tamil-month-panchangam.html?date=" + today
-
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-textCurrentDate = soup.find("h2").text
-
-data_items = ["Sunrise","Sunset","Nakshathram","Tithi","Rahu Kalam","Gulikai Kalam","Yamaganda"]
-# data_values = []
-
-app = tk.Tk()
-app.title("Daily Panchangam")
-app.resizable(False, False)
-
-divTamilDate = soup.find("div", {"class": "dpPHeaderLeftTitle"})
-
-textTamilDate = divTamilDate.text
-
-detailDivs = divTamilDate.find_next_siblings("div")
-
-textTamilDateDetails = ""
-
-for div in detailDivs:
-    textTamilDateDetails += div.text + " "
-                     
-#print(textTamilDate)
-#print(textTamilDateDetails)
-
-frmDate = tk.Frame(master=app)
-frmDate.grid(row=1,column=1, columnspan=4, padx=5, pady=5)
-lblDate =  tk.Label(master=frmDate,text=textCurrentDate)
-lblDate.config(font=("Helvetica",40))
-lblDate.pack()
-
-frmTamilDate = tk.Frame(master=app)
-frmTamilDate.grid(row=2,column=1, columnspan=4, padx=5, pady=5)
-lblTamilDate =  tk.Label(master=frmTamilDate,text=textTamilDate)
-lblTamilDate.config(font=("Helvetica",22))
-lblTamilDate.pack()
-
-frmTamilDateDetails = tk.Frame(master=app)
-frmTamilDateDetails.grid(row=3,column=1, columnspan=4, padx=5, pady=5)
-lblTamilDateDetails =  tk.Label(master=frmTamilDateDetails,text=textTamilDateDetails)
-lblTamilDateDetails.config(font=("Helvetica",16))
-lblTamilDateDetails.pack()
-
-row_num=4
-col_num=0
-
-for item in data_items:
-    item_data = find_data(item)
-    # data_values.append(find_data(item))
+    frmDate = None
+    lblDate = None
     
-    col_num += 1
-    frmItem = tk.Frame(master=app,relief=tk.FLAT,borderwidth=1)
-    frmItem.grid(row=row_num,column=col_num, padx=5, pady=5)
-    lblItem = tk.Label(master=frmItem,text=item)
-    lblItem.config(font="Helvetica 12 bold") 
-    lblItem.pack()
+    frmTamilDate = None
+    lblTamilDate = None
     
-    col_num += 1
-    frmItemData = tk.Frame(master=app,relief=tk.FLAT,borderwidth=1)
-    frmItemData.grid(row=row_num,column=col_num, padx=5, pady=5)
-    lblItemData = tk.Label(master=frmItemData,text=item_data)
-    lblItemData.config(font="Helvetica 12") 
-    lblItemData.pack()
+    frmTamilDateDetails = None
+    lblTamilDateDetails = None
+   
+    dataItems = ["Sunrise","Sunset","Nakshathram","Tithi",
+                 "Rahu Kalam","Gulikai Kalam","Yamaganda"]
+    dataValues = []
+    dataLabels = []
     
-    # row_num += 1
-    if col_num == 4:
-        row_num += 1
-        col_num = 0
+    def __init__(self, parent, day):
+        
+        tk.Frame.__init__(self, parent)
+        
+        self.date = day
+        
+        self.frames = []
+        
+        self.show_fields()
+        
+        self.fetch_data()
+        
+        self.show_data()
+        
+        
+    def show_fields(self):
+        
+        self.grid_columnconfigure(0, minsize=40, weight=1)
+        self.grid_columnconfigure(1, minsize=250, weight=1)
+        
+        self.grid_columnconfigure(2, minsize=40, weight=1)
+        self.grid_columnconfigure(3, minsize=250, weight=1)
+        
+        self.grid_rowconfigure(4, minsize=30, weight=1)
+        
+        self.grid_rowconfigure(5, minsize=50, weight=1)
+        self.grid_rowconfigure(6, minsize=30, weight=1)
+        self.grid_rowconfigure(7, minsize=20, weight=1)
+        
+        textCurrentDate = "Date"
+        textTamilDate = "Tamil Date"
+        textTamilDateDetails = "Tamil Date Details"
+        
+        frmDate = tk.Frame(master=self)
+        frmDate.grid(row=0,column=0, columnspan=4, padx=5, pady=5)
+        lblDate =  tk.Label(master=frmDate,text=textCurrentDate, justify=tk.LEFT)
+        lblDate.config(font=("Helvetica",40))
+        lblDate.pack()
+        
+        self.lblDate = lblDate
+
+        frmTamilDate = tk.Frame(master=self)
+        frmTamilDate.grid(row=1,column=0, columnspan=4, padx=5, pady=5)
+        lblTamilDate =  tk.Label(master=frmTamilDate,text=textTamilDate, justify=tk.LEFT)
+        lblTamilDate.config(font=("Helvetica",20))
+        lblTamilDate.pack()
+        
+        self.lblTamilDate = lblTamilDate
+
+        frmTamilDateDetails = tk.Frame(master=self)
+        frmTamilDateDetails.grid(row=2,column=0, columnspan=4, padx=5, pady=5)
+        lblTamilDateDetails =  tk.Label(master=frmTamilDateDetails,text=textTamilDateDetails, justify=tk.LEFT)
+        lblTamilDateDetails.config(font=("Helvetica",16))
+        lblTamilDateDetails.pack()
+        
+        self.lblTamilDateDetails = lblTamilDateDetails
+        
+        row_num=3
+        col_num=-1
+        
+        for item in self.dataItems:
+                  
+            col_num += 1
+            frmItem = tk.Frame(master=self,relief=tk.FLAT,borderwidth=1)
+            frmItem.grid(row=row_num,column=col_num, padx=5, pady=5, sticky=tk.W)
+            lblItem = tk.Label(master=frmItem,text=item, justify=tk.LEFT, wraplength=120)
+            lblItem.config(font="Helvetica 12 bold") 
+            lblItem.pack()
+            
+            col_num += 1
+            frmItemData = tk.Frame(master=self,relief=tk.FLAT,borderwidth=1)
+            frmItemData.grid(row=row_num,column=col_num, padx=5, pady=5, sticky=tk.W)
+            lblItemData = tk.Label(master=frmItemData,text="", justify=tk.LEFT, wraplength=250)
+            lblItemData.config(font="Helvetica 12") 
+            lblItemData.pack()
+            
+            self.dataLabels.append(lblItemData)
+            
+            if col_num == 3:
+                row_num += 1
+                col_num = -1
+            
+    def set_date(self,date):
+        
+        self.date = date
+        self.fetch_data_for_date(date)
+        self.show_data()
+        
+    def fetch_data(self):
+        
+        self.fetch_data_for_date(self.date)
+        
+    def fetch_data_for_date(self, date):
+        
+        dateValue = date.strftime("%d/%m/%Y")
+        
+        URL = "https://www.drikpanchang.com/tamil/tamil-month-panchangam.html?geoname-id=4684888&date=" + dateValue
+
+        page = requests.get(URL)
+
+        self.soup = BeautifulSoup(page.content, "html.parser")
+        
+        self.textCurrentDate = self.date.strftime("%a %b %d, %Y")
+        
+        divTamilDate = self.soup.find("div", {"class": "dpPHeaderLeftTitle"})
+
+        self.textTamilDate = divTamilDate.text
+
+        detailDivs = divTamilDate.find_next_siblings("div")
+
+        self.textTamilDateDetails = ""
+
+        for div in detailDivs:
+            self.textTamilDateDetails += div.text + " "
+        
+        self.dataValues = []
+        
+        for item in self.dataItems:
+            item_data = self.find_data(item)
+            self.dataValues.append(item_data)
+                
+    def find_data(self, str_type):
     
-frmBtnPrev = tk.Frame(master=app)
-frmBtnPrev.grid(row=8,column=1,padx=5, pady=5)
-btnPrev = tk.Button(master=frmBtnPrev, text="<", command=showPrevDate)
-btnPrev.pack()
+        return_text = ""
+        results = self.soup.find(text=str_type).parent
+        
+        if results.name != "span":
+            results = results.parent
 
-frmBtnNext = tk.Frame(master=app)
-frmBtnNext.grid(row=8,column=2,padx=5, pady=5)
-btnNext = tk.Button(master=frmBtnNext, text=">", command=showNextDate)
-btnNext.pack()
+        spans = results.find_next_siblings("span")
+
+        for span in spans:
+            return_text += span.text
+            
+        return return_text
 
 
-# print(data_values)
+    def show_data(self):
+        
+        self.lblDate.configure(text=self.textCurrentDate)
+        self.lblTamilDate.configure(text=self.textTamilDate)
+        self.lblTamilDateDetails.configure(text=self.textTamilDateDetails)
+                            
+        for idx, item in enumerate(self.dataItems):
+        
+            self.dataLabels[idx].configure(text=self.dataValues[idx])
+        
+            
+app = Panchangam()
 app.mainloop()
-
-  
-
-
-
-
-
