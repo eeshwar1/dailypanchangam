80a34b98443199d78bb0b478d15aed06c8fe602d
Venky Venkatakrishnan <venkyul@gmail.com>
2023-07-29 16:51:19 -0500

    Add files via upload

--- dailypanchangam.py
+++ dailypanchangam.py
@@ -249,7 +249,7 @@ class dayFrame(tk.Frame):
         location.set('Dallas') # set the default option
 
         locationPopupStyle = ttk.Style()
-        locationPopupStyle.configure("my.TMenubutton",font=("Helvetica",int(8 * self.size_ratio)),width=int(15 * self.size_ratio))
+        locationPopupStyle.configure("my.TMenubutton",font=("Helvetica",int(8 * self.size_ratio)),width=int(18 * self.size_ratio))
         frmLocationPopup = tk.Frame(master=self)
         frmLocationPopup.grid(row=5, column=0, padx=20, pady=5, sticky=tk.EW)
         locationPopup = ttk.OptionMenu(frmLocationPopup, location, *locations, style="my.TMenubutton", command=self.set_location)
